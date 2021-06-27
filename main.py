e1066a5cdbd57ad09bd1de9a499d401ff8188ed4
Marek <61633829+marek-kaminski@users.noreply.github.com>
2021-06-27 21:24:14 +0200

    Update main.py
    Added a fixed width to buttons

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from functools import partial
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
 from STALKER_RPG import stalker_rpg
-
+import random
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
 
@@ -23,22 +24,31 @@ class MyGrid(GridLayout):
     """coment"""
 
 
+
+
+
 class WarhammerApp (App):
     def build(self):
-        mylayout = GridLayout(cols=3, rows=3, padding = 50)
+        mylayout = GridLayout(cols=3, rows=3, padding = 50,  row_default_height=40)
         mylabel = Label(text= "My App", pos_hint={'center_x': 0.5 })
         printed_text = Label(text="hi")
-        Button1 = Button(text="DmD 5e",  pos_hint={'center_x': 0.2, 'center_y': 0.2})
-        Button2 = Button(text="Warhammer 4ed",  pos_hint={'center_x': 0.2, 'center_y': 0.2})
-        Button3 = Button(text="S.T.A.L.K.E.R RPG",  pos_hint={'center_x': 0.2, 'center_y': 0.2})
+        Button1 = Button(text="DmD 5e",   size_hint_x=None, width=150, height=100)
+        Button2 = Button(text="Warhammer 4ed",  size_hint_x=None, width=150, height=100)
+        Button3 = Button(text="S.T.A.L.K.E.R RPG", size_hint_x=None, width=150, height=100)
+        asa = 'Hello world'
+        textinput = TextInput(text=asa)
+
+        def change_text():
+            print(12)
 
         Button1.bind(on_press= lambda a: print(mylabel.text))
-        Button2.bind(on_press= lambda a: print(mylabel.text))
+        Button2.bind(on_press= lambda a: change_text())
         Button3.bind(on_press= lambda a: stalker_rpg())
         mylayout.add_widget(Button1)
         mylayout.add_widget(Button2)
         mylayout.add_widget(Button3)
         mylayout.add_widget(printed_text)
+        mylayout.add_widget(textinput)
         return mylayout
 
 
