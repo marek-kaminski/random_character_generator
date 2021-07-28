46ae80f98135750e87b064343eb1ca79db550f0a
marek <marek.kaminski95@gmail.com>
2021-07-28 21:37:29 +0200

    Started working on a new front

--- main.py
+++ main.py
@@ -1,56 +1,12 @@
-
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
-from random import random as r
-from functools import partial
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.gridlayout import GridLayout
-from STALKER_RPG import stalker_rpg
-import random
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uxi.widget import Widget
 
 
+class MainWidget(Widget):
+    pass
 
 
-class MyGrid(GridLayout):
-    """coment"""
+class TheLabApp(App):
+    pass
 
-
-
-
-
-class WarhammerApp (App):
-    def build(self):
-        mylayout = GridLayout(cols=3, rows=3, padding = 50,  row_default_height=40)
-        mylabel = Label(text= "My App", pos_hint={'center_x': 0.5 })
-        printed_text = Label(text="hi")
-        Button1 = Button(text="DmD 5e",   size_hint_x=None, width=150, height=100)
-        Button2 = Button(text="Warhammer 4ed",  size_hint_x=None, width=150, height=100)
-        Button3 = Button(text="S.T.A.L.K.E.R RPG", size_hint_x=None, width=150, height=100)
-        asa = 'Hello world'
-        textinput = TextInput(text=asa)
-
-        def change_text():
-            print(12)
-
-        Button1.bind(on_press= lambda a: print(mylabel.text))
-        Button2.bind(on_press= lambda a: change_text())
-        Button3.bind(on_press= lambda a: stalker_rpg())
-        mylayout.add_widget(Button1)
-        mylayout.add_widget(Button2)
-        mylayout.add_widget(Button3)
-        mylayout.add_widget(printed_text)
-        mylayout.add_widget(textinput)
-        return mylayout
-
-
-if __name__ == "__main__":
-    WarhammerApp().run()
+TheLabApp().run()
