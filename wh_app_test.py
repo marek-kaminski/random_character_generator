82382a42f505eee755e7b96496ea2c2616fd4662
marek <marek.kaminski95@gmail.com>
2021-07-28 21:35:01 +0200

    removed excessive code

--- wh_app_test.py
+++ wh_app_test.py
@@ -1,3 +1,11 @@
+import sys
+from unittest import TestCase
 
-import unittest
+#example "hello word" test #
+
+class Evaluate(TestCase):
+    def test_hello_world(self):
+        import exercise  # Imports and runs student's solution
+        output = sys.stdout.getvalue()  # Returns output since this function started
+        self.assertEqual("Hello World\n", output, "You must print Hello World")
 
