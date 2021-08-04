5e8827bbfc1dc91afca29374987d3736b4fa0a6c
marek <marek.kaminski95@gmail.com>
2021-08-04 22:07:24 +0200

    Added code that will be later used for changing the Label text

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from kivy.uix.scrollview import ScrollView
 from STALKER_RPG import stalker_rpg
 from STALKER_RPG import simple_printing
 
-random_number= ("this garbage works!!")
 
 class BoxLayoutExample(BoxLayout):
     my_text = StringProperty("a")
     def on_button_click(self):
         #need to add the "self." property before my_text, otherwise it will be a local variable
-        self.my_text = "garbage"
 
+        # stalker_rpg()
+        self.my_text = str(stalker_rpg())
+        # simple_printing()
 
 # class MainWidget(Widget):
 #     pass
